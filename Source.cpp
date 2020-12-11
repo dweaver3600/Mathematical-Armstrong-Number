b4142c6cc4f47320c1ec61077d77bcb6d0a9f36d
Dominick Weaver <dweaver3600@yahoo.com>
2020-12-10 18:39:48 -0800

    Update Source.cpp
    Made output more human readable.

--- Source.cpp
+++ Source.cpp
@@ -15,8 +15,12 @@
 
 int main() {
 
+	std::cout << "An Armstrong number of three digits is an integer such that the sum of the cubes\n"
+		<< "of its digits is equal to the number itself.\n\n\n";
+
 	std::string number;
 
+	std::cout << "Find if this THREE DIGIT number is an Armstrong number: ";
 	std::cin >> number;
 
 	int hunds = number[0] - '0';
@@ -25,10 +29,18 @@ int main() {
 
 	bool armstrong = false;
 
-	if (pow(hunds, 3) + pow(tens, 3) + pow(ones, 3) == std::stoi(number))
-		armstrong = true;
+	std::cout << "\n\n";
+	std::cout << hunds << "e3 + " << tens << "e3 + " << ones << "e3 = " << pow(hunds, 3) + pow(tens, 3) + pow(ones, 3);
 
-	std::cout << armstrong;
+	std::cout << "\n\n\n" << number;
+
+	if (pow(hunds, 3) + pow(tens, 3) + pow(ones, 3) == std::stoi(number)) {
+		std::cout << " IS";
+	}
+		
+	else std::cout << " IS NOT";
+
+	std::cout << " an Armstrong number.";
 
 	PAUSE
 	return 0;
